28f0b2445fb0252b41f5181f8113bab6ecebdb9d
DylanOrr-Uni <contact@dylanorr.com>
2024-05-17 18:24:42 -0400

    Completed V1 of the showcase

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! This example showcases a drawing a quad.
 mod quad {
+    use iced::advanced::graphics::color;
     use iced::advanced::layout::{self, Layout};
     use iced::advanced::renderer;
     use iced::advanced::widget::{self, Widget};
@@ -7,14 +8,16 @@ mod quad {
     use iced::{Border, Color, Element, Length, Rectangle, Shadow, Size};
 
     pub struct CustomQuad {
+        color: Color,
         size: f32,
         radius: [f32; 4],
         shadow: Shadow,
     }
 
     impl CustomQuad {
-        pub fn new(size: f32, radius: [f32; 4], shadow: Shadow) -> Self {
+        pub fn new(color: Color, size: f32, radius: [f32; 4], shadow: Shadow) -> Self {
             Self {
+                color,
                 size,
                 radius,
                 shadow,
@@ -62,7 +65,7 @@ mod quad {
                     },
                     shadow: self.shadow,
                 },
-                Color::from_rgba(1.0, 1.0, 1.0, 0.5),
+                self.color,
             );
         }
     }
@@ -76,11 +79,13 @@ mod quad {
 
 use std::default;
 
-use iced::border::Radius;
+use iced::border::{self, Radius};
 use iced::theme::{self, Theme};
 use iced::widget::container::Appearance;
-use iced::widget::{button, checkbox, column, container, horizontal_space, row, slider, text};
-use iced::{application, Border, Shadow, Vector};
+use iced::widget::{
+    button, checkbox, column, container, horizontal_rule, horizontal_space, row, slider, text,
+};
+use iced::{application, Border, Font, Shadow, Vector};
 use iced::{gradient, window};
 use iced::{Alignment, Background, Color, Element, Length, Radians, Sandbox, Settings};
 
@@ -104,6 +109,7 @@ struct Minimal {
     shadowed: bool,
     shadow: Shadow,
     radius: [f32; 4],
+    quad_color: Color,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -111,7 +117,10 @@ enum Message {
     StartChanged(Color),
     EndChanged(Color),
     AngleChanged(Radians),
-    ShadowToggled(bool),
+    ShadowColorChanged(Color),
+    ShadowOffsetXChanged(f32),
+    ShadowOffsetYChanged(f32),
+    QuadColorChanged(Color),
 }
 
 impl Sandbox for Minimal {
@@ -129,11 +138,12 @@ impl Sandbox for Minimal {
                 blur_radius: 16.0,
             },
             radius: [50.0; 4],
+            quad_color: Color::from_rgba(1.0, 1.0, 1.0, 0.5),
         }
     }
 
     fn title(&self) -> String {
-        String::from("Gradient")
+        String::from("Iced Widget Showcase")
     }
 
     fn update(&mut self, message: Message) {
@@ -141,7 +151,14 @@ impl Sandbox for Minimal {
             Message::StartChanged(color) => self.start = color,
             Message::EndChanged(color) => self.end = color,
             Message::AngleChanged(angle) => self.angle = angle,
-            Message::ShadowToggled(shadowed) => self.shadowed = shadowed,
+            Message::ShadowColorChanged(color) => self.shadow.color = color,
+            Message::QuadColorChanged(color) => self.quad_color = color,
+            Message::ShadowOffsetXChanged(x) => {
+                self.shadow.offset.x = x;
+            }
+            Message::ShadowOffsetYChanged(y) => {
+                self.shadow.offset.y = y;
+            }
         }
     }
 
@@ -153,6 +170,7 @@ impl Sandbox for Minimal {
             shadowed,
             shadow,
             radius,
+            quad_color,
         } = *self;
 
         // let gradient_box = container(horizontal_space())
@@ -169,11 +187,13 @@ impl Sandbox for Minimal {
         //             ..Default::default()
         //         }
         //     });
+        //
+        let ang = format!("{angle:.4}");
 
         let angle_picker = row![
             text("Angle").width(64),
             slider(Radians::RANGE, self.angle, Message::AngleChanged).step(0.01),
-            text(format!(" {:.2}", self.angle)),
+            text(format!("{ang:.4}")),
         ]
         .spacing(8)
         .padding(8)
@@ -181,7 +201,13 @@ impl Sandbox for Minimal {
 
         column![
             column![
-                row![text("Gradient")].padding(8).spacing(8),
+                row![text("Gradient").font(Font {
+                    weight: iced::font::Weight::Bold,
+                    ..Default::default()
+                })]
+                .padding(8)
+                .spacing(8),
+                container(horizontal_rule(0)).padding(8),
                 row![
                     column![
                         color_picker("Start", self.start).map(Message::StartChanged),
@@ -190,16 +216,74 @@ impl Sandbox for Minimal {
                     ],
                     // container(gradient_box),
                 ],
+                container(horizontal_rule(0)).padding(8),
+                row![text("Boxes").font(Font {
+                    weight: iced::font::Weight::Bold,
+                    ..Default::default()
+                })]
+                .padding(8)
+                .spacing(8),
+                column![
+                    color_picker("Color:", self.quad_color).map(Message::QuadColorChanged),
+                    color_picker("Shadow:", self.shadow.color).map(Message::ShadowColorChanged),
+                    row![
+                        text("Offset: ").width(64),
+                        row![
+                            text("X: "),
+                            slider(
+                                -100.0..=100.0,
+                                self.shadow.offset.x,
+                                Message::ShadowOffsetXChanged
+                            )
+                            .step(0.01),
+                            text(format!(" {:.2}", self.shadow.offset.x)),
+                        ],
+                        row![
+                            text("Y: "),
+                            slider(
+                                -50.0..=100.0,
+                                self.shadow.offset.y,
+                                Message::ShadowOffsetYChanged
+                            )
+                            .step(0.01),
+                            text(format!(" {:.2}", self.shadow.offset.y)),
+                        ],
+                    ]
+                    .padding(8)
+                    .spacing(8),
+                ],
+                // container(horizontal_rule(0)).padding(8),
+                // row![text("Cursor Interactions").font(Font {
+                //     weight: iced::font::Weight::Bold,
+                //     ..Default::default()
+                // })]
+                // .padding(8)
+                // .spacing(8),
             ],
             container(
                 row![
                     container(horizontal_space()),
-                    container(quad::CustomQuad::new(200.0, self.radius, self.shadow))
-                        .padding([100, 0]),
-                    container(quad::CustomQuad::new(200.0, self.radius, self.shadow))
-                        .padding([150, 50]),
-                    container(quad::CustomQuad::new(200.0, self.radius, self.shadow))
-                        .padding([200, 25]),
+                    container(quad::CustomQuad::new(
+                        self.quad_color,
+                        200.0,
+                        self.radius,
+                        self.shadow
+                    ))
+                    .padding([50, 0]),
+                    container(quad::CustomQuad::new(
+                        self.quad_color,
+                        200.0,
+                        self.radius,
+                        self.shadow
+                    ))
+                    .padding([100, 50]),
+                    container(quad::CustomQuad::new(
+                        self.quad_color,
+                        200.0,
+                        self.radius,
+                        self.shadow
+                    ))
+                    .padding([150, 25]),
                     container(horizontal_space()),
                 ]
                 .spacing(-200.0)
@@ -245,6 +329,12 @@ fn color_picker(label: &str, color: Color) -> Element<'_, Color> {
             slider(0.0..=1.0, color.a, move |a| { Color { a, ..color } }).step(0.01),
             text(format!(" {:.2}", color.a)),
         ],
+        quad::CustomQuad::new(
+            Color::from_rgba(color.r, color.g, color.b, color.a),
+            20.0,
+            [4.0, 4.0, 4.0, 4.0],
+            Shadow::default(),
+        ),
     ]
     .spacing(8)
     .padding(8)
